17109bcd9b9a08484e0f7db35939f3bf1ebfe0c4
Michael Hagen <michaelphagen@users.noreply.github.com>
2023-07-27 23:18:42 -0400

    remove util

--- util.py
+++
@@ -1,8 +0,0 @@
-import os
-import json
-
-def createManifest(games):
-    # Create manifest
-    with open(os.path.join(os.getcwd(),"manifest.json"), 'w') as file:
-        file.write(json.dumps(games))
-    return games
